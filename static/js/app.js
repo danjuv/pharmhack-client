5f72465dd70d48a574387878e2b31c867e7350f9
Danju <dvis5462@bitbucket.com>
2016-04-17 03:01:33 +1000

    added sales data and trend data assets

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,23 @@
-$($(document).ready(function() {
-  $.get("/api/recommendedOrders")
-});)
+$(document).ready(function() {
+  var date1 = $('#date1').val();
+  var date2 = $('#date2').val();
+  $.get("http://192.168.0.6/api/recommendedOrders?dateFrom="+date1+"&dateTo="+date2, function(data) {
+      highestProduct = "placeolder";
+      highestProductCost = 87;
+      lowestProduct = "Placeyounger";
+      lowestProductCost = 17;
+      highestTrend = "placeolder";
+      lowestTrend = "placeolder"
+      highestTrendCost = 141;
+      lowestTrendCost = 44;
+      $('#highest-cost').text(highestProductCost);
+      $('#highest-name').text(highestProduct);
+      $('#lowest-name').text(lowestProduct);
+      $('#lowest-cost').text(lowestProductCost);
+      $('#trending-highest-product').text(highestTrend);
+      $('#lowest-trending-name').text(lowestTrend);
+      $('#trending-highest-cost').text(highestTrendCost)
+      $('#trending-lowest-cost').text(lowestTrendCost)``
+
+  });
+})
